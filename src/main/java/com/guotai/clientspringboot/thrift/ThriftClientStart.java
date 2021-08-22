cbe9216f6c7d0a653b7d2772393bfac9abd1642d
Scallopwang <18052106678@163.com>
2021-08-22 18:46:17 +0800

    已添加进程，调整前端界面0822

--- src/main/java/com/guotai/clientspringboot/thrift/ThriftClientStart.java
+++ src/main/java/com/guotai/clientspringboot/thrift/ThriftClientStart.java
@@ -25,27 +25,33 @@ public class ThriftClientStart {
     @Autowired
     GetAgentInfoService getAgentInfoService;
 
-    private Agent agent = new Agent();
-    private AgentProcess agentProcess = new AgentProcess();
-    private AgentService.Client client = clientInit();
-
-    public AgentService.Client clientInit() {
-        try {
-            TFramedTransport transport = new TFramedTransport(new TSocket("localhost", 8888), 600);
-            TCompactProtocol tCompactProtocol = new TCompactProtocol(transport);
-            AgentService.Client client = new AgentService.Client(tCompactProtocol);
-            transport.open();
-            return client;
-        } catch (TTransportException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+//    private Agent agent = new Agent();
+//    private AgentProcess agentProcess = new AgentProcess();
+//    private AgentService.Client client = clientInit();
+//
+//    public AgentService.Client clientInit() {
+//        try {
+//            TFramedTransport transport = new TFramedTransport(new TSocket("localhost", 8888), 600);
+//            TCompactProtocol tCompactProtocol = new TCompactProtocol(transport);
+//            AgentService.Client client = new AgentService.Client(tCompactProtocol);
+//            return client;
+//        } catch (TTransportException e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
 
 
     public void clientStart() {
         try {
+            Agent agent = new Agent();
+            AgentProcess agentProcess = new AgentProcess();
+            TFramedTransport transport = new TFramedTransport(new TSocket("localhost", 8888), 600);
+            TCompactProtocol tCompactProtocol = new TCompactProtocol(transport);
+            AgentService.Client client = new AgentService.Client(tCompactProtocol);
             List processList = getAgentInfoService.getProcess();
+            transport.open();
+
             agent.setIp(getAgentInfoService.getIp());
             agent.setCpu_free(getAgentInfoService.getCPUInfo());
             agent.setMemory_free(getAgentInfoService.getMemInfo());
@@ -57,6 +63,8 @@ public class ThriftClientStart {
             agentProcess.setProcess_mem(processList.get(4).toString());
             agentProcess.setProcess_start_time(processList.get(2).toString());
             System.out.println("agent传输进程信息为：" + client.sendAgentProcessByIP(agentProcess.ip, agentProcess));
+            transport.close();
+
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -66,8 +74,14 @@ public class ThriftClientStart {
 
     public void agentCommandResBack(String msg) {
         try {
+            TFramedTransport transport = new TFramedTransport(new TSocket("localhost", 8888), 600);
+            TCompactProtocol tCompactProtocol = new TCompactProtocol(transport);
+            AgentService.Client client = new AgentService.Client(tCompactProtocol);
+            transport.open();
             // agent to server msg
             client.sendMsg(msg);
+            transport.close();
+
         } catch (Exception e) {
             e.printStackTrace();
             throw new RuntimeException(e.getMessage(), e);
